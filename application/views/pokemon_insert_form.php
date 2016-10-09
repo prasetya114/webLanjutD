c7b989d1f31465011a30ba117b081c41bf3f6408
Fiqih Risky Saputra <zhangfiq@gmail.com>
2016-10-09 12:33:36 +0700

    Update pokemon_insert_form.php

--- application/views/pokemon_insert_form.php
+++ application/views/pokemon_insert_form.php
@@ -1,2 +1,8 @@
 <?php
 /* Buat form untuk insert dengan metode post, yang akan dikirim ke pokemon/insert_action */
+?>
+<form name="frm_insert_pokemon" method='POST' action="<?=site_url('pokemon/insert_action')?>">
+ <input type="text" name="nama"><br>
+  <input type="text" name="tipe"><br>
+  <button name="simpan"> Tambah Pokemon</button>
+</form>
